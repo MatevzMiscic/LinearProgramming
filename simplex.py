5e375325c7d4bb9e700415bda2b9e03e8242bbaa
MatevzMiscic <matevz.miscic@gmail.com>
2020-11-18 17:19:46 +0100

    Preimenoval datoteke

--- simplex.py
+++ simplex.py
@@ -1,3 +1,5 @@
+from fraction import frac
+
 class Tableau:
     def __init__(self, A, b, c):
         self.n = len(c)
@@ -6,20 +8,20 @@ class Tableau:
         self.base = [self.n + i for i in range(self.m)]
         self.table = []
         for i in range(self.m):
-            row = A[i][::]
+            row = [frac(a) for a in A[i]]
             for j in range(self.m):
-                row.append(int(i == j))
-            row.append(b[i])
+                row.append(frac(int(i == j)))
+            row.append(frac(b[i]))
             self.table.append(row)
-        row = c[::]
-        for i in range(self.m + 1): row.append(0)
+        row = [frac(v) for v in c]
+        for i in range(self.m + 1): row.append(frac(0))
         self.table.append(row)
 
     def base_val(self):
         return -self.table[-1][-1]
 
     def base_sol(self):
-        solution = [0 for i in range(self.n + self.m + 1)]
+        solution = [frac(0) for i in range(self.n + self.m + 1)]
         solution[-1] = self.base_val()
         for i, var in enumerate(self.base):
             solution[self.base[i]] = self.table[i][-1]
@@ -61,9 +63,12 @@ class Tableau:
             return False
         return col
 
-    def leaving_rule(self, col, row=0):
-        # izberemo izstopajočo spremenljivko (oziroma vrstico)
+    def leaving_rule(self, col, out=None):
         row = 0
+        if out is not None:
+            for i, var in enumerate(self.base):
+                if var == out: row = i
+        # izberemo izstopajočo spremenljivko (oziroma vrstico)
         for i in range(self.m):
             if self.table[i][col] <= 0:
                 continue 
@@ -95,44 +100,38 @@ class Tableau:
         self.base[row] = col
         return True
 
-    def iterate(self):
+    def iterate(self, out=0):
         col = self.entering_rule()
         # če smo končali
         if col is False:
-            print("Konec")
             return False
         # če je problem neomejen
-        row = self.leaving_rule(col)
+        row = self.leaving_rule(col, out)
         if row is False:
             print("Neomejen problem")
             return False
         return self.pivot(row, col)
 
-    def simplex(self):
-        while self.iterate():
-            self.print()
+    def simplex(self, out=0):
+        while self.iterate(out):
             print()
+            self.print()
 
-    def tab(self):
-        print(self.table)
-    def __sign(self, num):
-        if num >= 0:
-            return " + "
-        return " - "
     def print(self):
         for i, k in enumerate(self.base):
-            print(f"x{k + 1} = {self.table[i][-1]:.2f}", end="")
+            #print(f"x{k + 1} = {self.table[i][-1]:.2f}", end="")
+            print(f"x{k + 1} = " + self.table[i][-1].unsignstr(), end="")
             for j in range(self.n + self.m):
                 if self.in_base[j]: continue
                 coef = -self.table[i][j]
-                print(self.__sign(coef) + f"{abs(coef):.2f} * x{j + 1}", end="")
+                print(coef.valstr() + f" * x{j + 1}", end="")
             print()
         val = -self.table[self.m][-1]
-        print(f"z  = {val}", end="")
+        print(f"z  = " + val.unsignstr(), end="")
         for j in range(self.n + self.m):
             if self.in_base[j]: continue
             coef = self.table[self.m][j]
-            print(self.__sign(coef) + f"{abs(coef):.2f} * x{j + 1}", end="")
+            print(coef.valstr() + f" * x{j + 1}", end="")
         print()
 
 def solve(A, b, c):
@@ -143,28 +142,33 @@ def solve(A, b, c):
         if b[i] < b[least]:
             least = i
     if b[least] < 0:
-        # bdr nedopustna, naredimo dvofazno metodo simpleksov
+        # br nedopustna, naredimo dvofazno metodo simpleksov
+        print("Začetni slovar je nodepusten.")
         for row in A:
             row.append(-1)
         d = [-int(i == n) for i in range(n + 1)]
         tab = Tableau(A, b, d)
         tab.pivot(least, n)
+        print("Reševanje prve faze metode simpleksov...")
         tab.print()
-        tab.simplex()
+        tab.simplex(n)
         if tab.base_val() < 0:
             return "Problem ni dopusten"
-        tab.del_var(n)
-        for i in range(m + 1): c.append(0)
+        assert tab.del_var(n)
+        c = [frac(v) for v in c]
+        for i in range(m + 1): c.append(frac(0))
         tab.express(c)
         tab.table[-1] = c[::]
+        print("Reševanje druge faze metode simpleksov...")
         tab.print()
         tab.simplex()
     else:
         # bdr dopustna
+        print("Začetni slovar je nodepusten.")
+        print("Reševanje problema z metodo simpleksov...")
         tab = Tableau(A, b, c)
-        tab.print()
-        print()
         tab.simplex()
+    print("Problem rešen.")
     return tab
 
 def lsb(n):
@@ -184,27 +188,31 @@ def goto(tab, key):
         for i, var in enumerate(tab.base):
             if var == a:
                 row = i
-        tab.pivot(row, b)
+        if tab.pivot(row, b) == False:
+            print("not pivoting")
+        if key == 145066 and curkey == 152250:
+            tab.print()
         curkey = tab.key()
         
 # poišče vse optimalne bdr (ne le tistih do katerih lahko pride simpleks)
-def findall(tab):
-    """
+"""def findall(tab):
     # odstranimo spremenljivke, ki bodo zagotovo enake 0
-    j = tab.n + tab.m - 1
-    while j >= 0:
-        if tab.table[tab.m][j] < 0:
-            tab.del_var(j)
-        j -= 1
-    """
+    #j = tab.n + tab.m - 1
+    #while j >= 0:
+    #    if tab.table[tab.m][j] < 0:
+    #        tab.del_var(j)
+    #    j -= 1
     # naredimo dfs po slovarjih
     solutions = set()
-    visited = set()
-    stack = [tab.key()]
+    visited = {}
+    visited[tab.key()] = -1
+    stack = [(tab.key(), 0)]
     while stack:
-        key = stack.pop()
+        key, idx = stack.pop()
         if key in visited: continue
-        print(len(visited), key)
+        print(f'{len(visited)}: {par} -> {key}')
+        if len(visited) == 116:
+            tab.print()
         visited.add(key)
         goto(tab, key)
         solutions.add(tab.base_sol())
@@ -214,11 +222,45 @@ def findall(tab):
             if row is False: continue
             newkey = (key & ~(1 << tab.base[row])) | (1 << j)
             if newkey not in visited:
-                stack.append(newkey)
+                print(f'    {key} -> {newkey}')
+                stack.append((newkey, key))
     tab.print()
+    return solutions"""
+
+def findall(tab):
+    # naredimo dfs po slovarjih
+    solutions = set()
+    visited = {}
+    visited[tab.key()] = -1
+    stack = [(tab.key(), 0)]
+    while stack:
+        #print(stack)
+        key, idx = stack.pop()
+        if idx == 0:
+            if visited[key] != -1:
+                row, col, _ = visited[key]
+                tab.pivot(row, col)
+                #tab.print()
+            #print(key == tab.key())
+            solutions.add(tab.base_sol())
+        for i in range(idx, tab.n + tab.m):
+            idx = i
+            if tab.in_base[i] or tab.table[tab.m][i] != 0: continue 
+            row = tab.leaving_rule(i)
+            if row is False:
+                print("Množica rešitev je neomejena")
+                return None
+            newkey = (key & ~(1 << tab.base[row])) | (1 << i)
+            if newkey in visited: continue
+            visited[newkey] = (row, i, tab.base[row])
+            stack.append((key, i + 1))
+            stack.append((newkey, 0))
+            break
+        else:
+            if visited[key] != -1:
+                row, _, col = visited[key]
+                tab.pivot(row, col)      
     return solutions
-
-
 
 
 
@@ -226,9 +268,9 @@ def findall(tab):
 A = [[2, 3, 1], [5, 7, 2], [9, 6, 3]]
 b = [6, 3, 8]
 c = [2, 1, 7]
-t = Tableau(A, b, c)
 tab = solve(A, b, c)
 #"""
+
 """
 A = [[1, 2], [5, 4]]
 b = [6, 20]
@@ -240,16 +282,18 @@ tab = solve(A, b, c)
 A = [[1, 1, 1], [60, 80, 100], [400, 600, 480]]
 b = [50, 5000, 24000]
 c = [240, 400, 320]
+tab = Tableau(A, b, c)
 tab = solve(A, b, c)
-
+tab.print()
 sols = findall(tab)
 #"""
 
-#"""
+"""
 A = [[-1, -7, 2], [5, 8, -6], [-9, 1, -2]]
 b = [3, 1, 4]
 c = [0, -1, 0]
 tab = solve(A, b, c)
+tab.print()
 sols = findall(tab)
 #"""
 
@@ -259,6 +303,7 @@ A = [[-1, 0, -2], [-1, -2, 1], [-1, 1, 0], [-1, 0, 1]]
 b = [-2, 1, 0, -1]
 c = [12, 6, 3]
 tab = solve(A, b, c)
+sols = findall(tab)
 #"""
 
 """
